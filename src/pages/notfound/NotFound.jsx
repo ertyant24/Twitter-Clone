e594c73a0cb65252e9cbe4370dd98207c619432b
ertyant24 <erikciertugrul@gmail.com>
2023-09-30 02:29:02 +0300

    notFound page revize

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -7,8 +7,8 @@ function NotFound() {
       <div className="container my-5">
         <div className="row">
             <div className="col-6 offset-3 text-center mt-5">
-                <h1 className='text-center mb-4 mt-3'>Not Found</h1>
-                <img className='rounded-circle' src={NotFoundImage} alt="" width="400px"/>
+                <h1 className='text-center mb-4 my-3 text-danger fw-semibold'>Not Found</h1><hr />
+                <img className='rounded-circle mt-4' src={NotFoundImage} alt="" width="400px"/>
             </div>
         </div>
       </div>
