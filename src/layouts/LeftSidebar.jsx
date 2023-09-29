129dd2c5c3d2b6682ff1f2ef3a61e350ec233885
ertyant24 <erikciertugrul@gmail.com>
2023-09-29 13:45:18 +0300

    revize

--- src/layouts/LeftSidebar.jsx
+++ src/layouts/LeftSidebar.jsx
@@ -4,7 +4,7 @@ import Logo from '../logo/Logo'
 function LeftSidebar() {
   return (
     <>
-      <aside className='px-2 pt-3' style={{width: "275px", minHeight: "100vh"}}>
+      <aside className='px-2 pt-2' style={{width: "275px", minHeight: "100vh"}}>
         <Logo/>
       </aside>
     </>
